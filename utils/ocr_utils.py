522e32f16f3b442ef189eed4dee535d02a39bfce
Meng-Hsueh Sung <meng-hsuehsung@MacBook-Air.local>
2024-11-07 00:31:08 +0800

    processing image and do more REGEX

--- utils/ocr_utils.py
+++ utils/ocr_utils.py
@@ -1,36 +1,58 @@
-import os
-
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageOps, ImageFilter, ImageEnhance
 import re
 
+
 def preprocess_image(image):
+    # Adjust Image direction
+    image = ImageOps.exif_transpose(image)
     # transit to grey image
     gray = image.convert('L')
-    # 二值化處理
-    threshold = 200
-    binary = gray.point(lambda x: 0 if x < threshold else 255, '1')
-    return binary
+    sharpened_image = gray.filter(ImageFilter.SHARPEN)
+    enhancer = ImageEnhance.Contrast(sharpened_image)
+    enhanced_image = enhancer.enhance(2.0)
+    # enhanced_image.save("example.png")  # Local debug use
+    return enhanced_image
 
 def extract_text_from_image(image_path, lang='chi_tra'):
     # open image
     image = Image.open(image_path)
     # preprocess image
-    # processed_image = preprocess_image(image)
+    processed_image = preprocess_image(image)
     # OCR
-    text = pytesseract.image_to_string(image, lang=lang)
+    text = pytesseract.image_to_string(processed_image, lang=lang)
+    text = text.replace('\n', '').strip()
+    text = text.replace(' ', '').strip()
     print("OCR result:" + text)
     return text
 
 def parse_total_amount(text):
-    # REGEX to get total amount
-    pattern = r'(总计|合计|金额|TOTAL|總計|合計|金額)\s*[:：]?\s*[￥¥$NT新臺幣]?\s*(\d+[,\d]*\.?\d*)[元]?'
-    matches = re.findall(pattern, text, re.IGNORECASE)
+    # First REGEX match
+    pattern1 = r'(总计|合计|金额|TOTAL|總計|合計|金額)\s*[:：]?\s*[￥¥$＄NT新臺幣]?\s*(\d+[,\d]*\.?\d*)'
+    matches = re.findall(pattern1, text, re.IGNORECASE)
     if matches:
-        # get last matched group
-        amount = matches[-1][1]
-        # delete possible comma
-        amount = amount.replace(',', '')
+        amount = matches[-1][1].replace(',', '')
         return amount
     else:
-        return "無法識別總金額"
+        # Second REGEX match, considering replacing general OCR error
+        text_corrected = text.replace('S', '$').replace('%', '$')
+        pattern2 = r'(总计|合计|金额|TOTAL|總計|合計|金額)\s*[:：]?\s*[￥¥$＄NT新臺幣$]?\s*(\d+[,\d]*\.?\d*)'
+        matches = re.findall(pattern2, text_corrected, re.IGNORECASE)
+        if matches:
+            amount = matches[-1][1].replace(',', '')
+            return amount
+        else:
+            # Try to match number again
+            text_corrected = text.replace('S', '$').replace('%', '$').replace('NT', '$')
+            pattern3 = r'[￥¥$＄S%]{1}\s*(\d+[,\d]*\.?\d*)'
+            matches = re.findall(pattern3, text_corrected)
+            if matches:
+                amount = matches[-1].replace(',', '')
+                return amount
+            else:
+                return "無法識別總金額"
+
+if __name__ == "__main__":
+    extract_text = extract_text_from_image("/.../xxx.JPG")
+    ans = parse_total_amount(extract_text)
+    print(ans)
